6fb5ea4771e419f03bb1f0b42aae898c251e2fa4
Afham2263 <126801750+Afham2263@users.noreply.github.com>
2025-08-08 23:04:37 +0530

    Update chains.py

--- app/chains.py
+++ app/chains.py
@@ -3,13 +3,15 @@ from langchain_groq import ChatGroq
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.exceptions import OutputParserException
-from dotenv import load_dotenv
-
-load_dotenv()
+import streamlit as st
 
 class Chain:
     def __init__(self):
-        self.llm = ChatGroq(temperature=0, groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.1-70b-versatile")
+        self.llm = ChatGroq(
+            temperature=0,
+            groq_api_key=st.secrets["GROQ_API_KEY"],
+            model_name="llama-3.1-70b-versatile"
+        )
 
     def extract_jobs(self, cleaned_text):
         prompt_extract = PromptTemplate.from_template(
@@ -55,6 +57,3 @@ class Chain:
         chain_email = prompt_email | self.llm
         res = chain_email.invoke({"job_description": str(job), "link_list": links})
         return res.content
-
-if __name__ == "__main__":
-    print(os.getenv("GROQ_API_KEY"))
